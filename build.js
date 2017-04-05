f8f65258fdefa0ea709b1ea25722111a8c391400
fotisz <fotisz@users.noreply.github.com>
2017-04-05 17:22:48 +0100

    Add files via upload

---
+++ build.js
@@ -0,0 +1,10 @@
+({
+    optimize: 'none',
+	baseUrl: '',
+    paths: {
+		'd3': 'd3.v3.min',
+		'd3_circle_packing': 'd3_circle_packing'
+    },
+    name: "d3_circle_packing",
+    out: "d3_circle_packing.min.js"
+})
